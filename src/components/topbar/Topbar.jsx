7c756cf81e1fd76634b49803bc1d83479d56bca6
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-07 16:46:17 -0700

    styling topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,21 @@
 import "./topbar.scss"
+import {Mail} from '@mui/icons-material';
+
 
 export default function Topbar() {
   return (
-    <div className="topbar">
-        
+    <div className="topbar" id="topbar">
+        <div className="wrapper">
+          <div className="left">
+            <a href="#intro" className="logo">&lt; / &gt;</a>
+              <div className="itemContainer">
+                <Mail className="icon"/>
+                <span>jackrobert0220@gmail.com</span>
+              </div>
+          </div>
+          <h1>Hello</h1>
+          <div className="right">This is right</div>
+        </div>
     </div>
   )
 }
