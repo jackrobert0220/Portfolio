aca1add422c21fa6ccc03e63a8b154dcf34309a0
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-07 22:12:35 -0700

    menu is functioning

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import "./portfolio.scss"
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="intro">
+    <div className="portfolio" id="portfolio">
         Portfolio
     </div>
   )
