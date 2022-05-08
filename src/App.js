f1a85c11a28910e1fc5501b44425aefbb7aadcc2
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-07 17:39:41 -0700

    adding structure to hamburger menu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/topbar/Topbar";
 import Works from "./components/works/Works";
 import "./app.scss";
+import Menu from './components/menu/Menu';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/> 
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
